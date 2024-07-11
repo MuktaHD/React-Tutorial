b228556f4b163087515b3820de189a039746c574
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-07-11 12:39:29 +0530

    src folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import {Fragment} from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+<h1 style={{textAlign:'center'}}>Hello world</h1>
+<p>This is a simple React app.</p>
+    </div>
+   
+
+  );
+}
+
+export default App;
